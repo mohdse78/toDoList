43d47b72a274eec0885b90d071a43542359e524b
mohaddeseh <mohadeseganjalinia@gmail.com>
2022-12-30 15:44:14 +0330

    Fix: vol 3

--- project/src/app/to-do-list/to-do-list.component.ts
+++ project/src/app/to-do-list/to-do-list.component.ts
@@ -42,8 +42,6 @@ export class ToDoListComponent implements OnInit {
   doingDraggedCartId: any;
   doneDraggedCartId: any;
   prioritySelected: any;
-  innerWidth: any= false;
-  mobileSize: boolean= false;
 
 
   duties: { id: number, title: string, description: string, date: string, priority: { id: number, name: string, code: string } }[] = []
@@ -52,13 +50,6 @@ export class ToDoListComponent implements OnInit {
 
 
   ngOnInit(): void {
-
-    this.innerWidth = window.innerWidth;
-    if (this.innerWidth < 1024) {
-      this.mobileSize = true
-    }
-
-
     this.newDuty = this.fb.group({
       id: [0],
       title: ['', [Validators.required]],
